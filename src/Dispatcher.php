5359bdbc97103c7f3300c196c624f4195d62ff1c
Jordy <arno.zheng@gmail.com>
2018-07-01 12:19:13 +0800

    fix: fix the default controller

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -25,7 +25,8 @@ class Dispatcher
 
         if ($routePath) {
             $controllerPath = $this->parseRoutePath($routePath);
-        } else {
+        }
+        if (!$controllerPath) {
             $controllerPath = 'Index';
         }
 
