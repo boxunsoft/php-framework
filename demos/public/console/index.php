eeee33b4f2f1c5d07fb1784a533aeb44810ddb61
Jordy <arno.zheng@gmail.com>
2018-12-26 19:02:29 +0800

    fix: remove namespace Ala from composer.json

--- demos/public/console/index.php
+++ demos/public/console/index.php
@@ -1,7 +1,22 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Jordy
+ * Date: 2018/12/25
+ * Time: 下午1:43
+ */
 
 $rootPath = dirname(dirname(__DIR__));
-require dirname($rootPath) . '/vendor/autoload.php';
+
+/**
+ * @var \Composer\Autoload\ClassLoader $loader
+ */
+$loader = require dirname($rootPath) . '/vendor/autoload.php';
+
+/*----- test start ------*/
+//App suggests adding composer.json
+$loader->addPsr4('Ala\\', $rootPath);
+/*----- test end ------*/
 
 $app = \Alf\Application::getInstance();
 $app->startup($rootPath, 'Console');
