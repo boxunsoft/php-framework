ed0789e985f378bc7296f4c5cb467a15862a2db2
zjd <zjd@meitu.com>
2018-12-26 19:02:29 +0800

    fix: remove namespace Ala from composer.json

--- demos/public/home/index.php
+++ demos/public/home/index.php
@@ -7,7 +7,16 @@
  */
 
 $rootPath = dirname(dirname(__DIR__));
-require dirname($rootPath) . '/vendor/autoload.php';
+
+/**
+ * @var \Composer\Autoload\ClassLoader $loader
+ */
+$loader = require dirname($rootPath) . '/vendor/autoload.php';
+
+/*----- test start ------*/
+//App suggests adding composer.json
+$loader->addPsr4('Ala\\', $rootPath);
+/*----- test end ------*/
 
 $suffixs = ['.json'];
 
