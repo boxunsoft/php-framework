39ab278f9aab10c63bebb962acaf365bbd09ca2f
zjd <zjd@meitu.com>
2018-12-27 12:06:56 +0800

    feat: add NotFoundException Class

---
+++ src/Exception/NotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jordy
+ * Date: 2018/12/25
+ * Time: 上午11:42
+ */
+
+namespace Alf\Exception;
+
+use Alf\Exception;
+
+class NotFoundException extends Exception
+{
+}
